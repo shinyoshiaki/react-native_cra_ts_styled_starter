d3c44f3c56ee85094c2a75454d651596fec56776
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-05-18 17:20:39 +0900

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,14 @@
-import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
-import styled from "styled-components/native";
+import React, { FC } from "react";
+import { View } from "react-native";
+import { Router, Route } from "./multi/router";
+import Main from "./pages/Main";
 
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
+const App: FC = () => (
+  <View>
+    <Router>
+      <Route exact path="/" component={Main} />
+    </Router>
+  </View>
+);
 
-const Color = styled.Text`
-  color: red;
-`;
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Color>Welcome to React Native! a</Color>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
-  }
-});
+export default App;
---
+++ src/multi/router/index.tsx
@@ -0,0 +1,9 @@
+import {
+  NativeRouter,
+  Route as NativeRoute,
+  Link as NativeLink
+} from "react-router-native";
+
+export let Router = NativeRouter;
+export let Route = NativeRoute;
+export let Link = NativeLink;
